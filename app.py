72cd3b2311e56b4ca1d1b3ab656a23efd293a10c
Bhaswati Roy <roybhaswati24@gmail.com>
2022-03-07 21:31:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ y2=np.array(y2)
 x2_train,x2_test,y2_train,y2_test=train_test_split(x2,y2,test_size=0.2,random_state=0)
 #creating an object for the model for further usage
 model2=RandomForestClassifier()
-#fitting the model with train data (x1_train & y1_train)
+#fitting the model with train data (x2_train & y2_train)
 model2.fit(x2_train,y2_train)
 
 #Diabetes Page
@@ -101,25 +101,39 @@ if rad=="Diabetes":
             st.success("You Are Safe")
 
 #Heart Disease Prediction
+
+#loading the Heart Disease dataset
 df3=pd.read_csv("Heart Disease Predictions.csv")
+#cleaning the data by dropping unneccessary column and dividing the data as features(x3) & target(y3)
 x3=df3.iloc[:,[2,3,4,7]].values
 x3=np.array(x3)
 y3=y3=df3.iloc[:,[-1]].values
 y3=np.array(y3)
+#performing train-test split on the data
 x3_train,x3_test,y3_train,y3_test=train_test_split(x3,y3,test_size=0.2,random_state=0)
+#creating an object for the model for further usage
 model3=RandomForestClassifier()
+#fitting the model with train data (x3_train & y3_train)
 model3.fit(x3_train,y3_train)
 
 #Heart Disease Page
+
+#heading over to the Heart Disease section
 if rad=="Heart Disease":
     st.header("Know If You Are Affected By Heart Disease")
     st.write("All The Values Should Be In Range Mentioned")
+    #taking the 4 most important features as input as features -> Chest Pain (chestpain), Blood Pressure-BP (bp), Cholestrol (cholestrol), Maximum HR (maxhr)
+    #a min value (min_value) & max value (max_value) range is set so that user can enter value within that range
+    #incase user enters a value which is not in the range then the value will not be taken whereas an alert message will pop up
     chestpain=st.number_input("Rate Your Chest Pain (1-4)",min_value=1,max_value=4,step=1)
     bp=st.number_input("Enter Your Blood Pressure Rate (95-200)",min_value=95,max_value=200,step=1)
     cholestrol=st.number_input("Enter Your Cholestrol Level Value (125-565)",min_value=125,max_value=565,step=1)
     maxhr=st.number_input("Enter You Maximum Heart Rate (70-200)",min_value=70,max_value=200,step=1)
+    #the variable prediction1 predicts by the health state by passing the 4 features to the model
     prediction3=model3.predict([[chestpain,bp,cholestrol,maxhr]])[0]
-
+    
+    #prediction part predicts whether the person is affected by Heart Disease or not by the help of features taken as input
+    #on the basis of prediction the results are displayed
     if st.button("Predict"):
         if str(prediction3)=="Presence":
             st.warning("You Might Be Affected By Diabetes")
